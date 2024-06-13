a7778436c1f81f221d7cfe62d8943058a56b8d66
Dominic Myers <annoyingmouse@gmail.com>
2024-06-13 16:20:22 +0100

    docs(main.js): add comments with inspiration links and description for the main entry point of the application

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
+/**
+ * Main entry point of the application
+ * Inspiration:
+ * https://github.com/maoberlehner/goodbye-webpack-building-vue-applications-without-webpack/blob/master/src/components/BaseButton.js
+ * https://codepen.io/xiaomingming/pen/yVmMOz
+ */
+
 import App from "./components/App.js";
 
 const app = Vue.createApp({
 	render: () => Vue.h(App),
 });
+
 app.mount("#app");
